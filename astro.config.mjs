1c6c64884e584dabaf6171790d70d4c2836d97a7
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2023-12-06 06:42:51 +0300

    upgrade to astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import solidJs from '@astrojs/solid-js';
@@ -8,13 +7,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.ronanru.com',
-  experimental: {
-    viewTransitions: true,
-  },
   integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
     mdx(),
     sitemap(),
     tailwind({
